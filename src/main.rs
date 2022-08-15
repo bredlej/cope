eb95272a4996f8dd216604cfcd1d63cc9d96fe57
Patryk Szczypień <patryk.szczypien@gmail.com>
2022-08-15 18:20:08 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use bevy::prelude::*;
+use bevy::render::texture::ImageSettings;
+
+mod components;
+mod world;
+mod resources;
+
+mod prelude {
+    pub use bevy::prelude::*;
+    pub use crate::components::*;
+    pub use crate::world::*;
+    pub use crate::resources::*;
+}
+
+use prelude::*;
+
+fn initialize(mut commands: Commands,
+              asset_server: Res<AssetServer>,
+              mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    println!("Main initialize");
+    let texture_handle = asset_server.load("ascii8x8.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(8.0, 8.0), 16, 16);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands.insert_resource(CharsetAsset { atlas: texture_atlas_handle.clone() });
+    commands.insert_resource(ImageSettings::default_nearest());
+    commands.spawn_bundle(Camera2dBundle::default());
+}
+
+fn run(mut game_state: ResMut<State<GameState>>) {
+    let current_state = game_state.current().clone();
+    game_state.set(GameState::BuildWorld);
+}
+
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "COPE".to_string(),
+            width: 800.0,
+            height: 800.0,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::from([0.0, 0.0, 0.0])))
+        .add_plugins(DefaultPlugins)
+        .add_plugin(WorldPlugin)
+        .add_startup_system(initialize)
+        .add_state(GameState::Initial)
+        .add_system_set(
+            SystemSet::on_update(GameState::Initial)
+                .with_system(run)
+        )
+        .run();
+}
